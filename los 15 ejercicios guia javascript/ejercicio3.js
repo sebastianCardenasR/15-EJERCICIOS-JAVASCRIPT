c3fa60578082118a8cb799c2d1832b18997bf7bf
sebastianCardenasR <rodrigueztatan4@gmail.com>
2016-12-14 13:04:30 -0500

    javascript EJERCICIOSGUIA
    15 ejercicios del punto 3.4.1

---
+++ los 15 ejercicios guia javascript/ejercicio3.js
@@ -0,0 +1,12 @@
+function area() {
+
+	var radio=document.getElementById('number1').value
+	var areat=3.14*(radio*radio);
+    alert("el valor del area ( A=PI*r^2) del circulo es:  "+ areat);
+
+};
+function longitud(){
+	var radio=document.getElementById('number1').value
+	var longitudt=radio*2;
+    alert("el valor de la longutid la circunferencia es :  "+ longitudt);
+}
